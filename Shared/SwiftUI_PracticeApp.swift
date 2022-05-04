10ddb39ec16bb9a7f1a2b87f144b9a76e59bd862
teddy <teddy@akaai.kr>
2022-05-04 11:09:07 +0900

    Initial Commit

---
+++ Shared/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  Shared
+//
+//  Created by Jong Won Moon on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
