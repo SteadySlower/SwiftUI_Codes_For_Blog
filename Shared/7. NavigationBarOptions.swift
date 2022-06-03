9ce3de3065434d93a6e2a7910c844beda2bcc3d8
jwmoon <moon910904@gmail.com>
2022-06-03 11:33:53 +0900

    gradient

---
+++ Shared/7. NavigationBarOptions.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBar.swift
+//  SwiftUI_Practice
+//
+//  Created by Jong Won Moon on 2022/06/03.
+//
+
+import SwiftUI
+
+struct NavigationBarOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarOptions_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarOptions()
+    }
+}
