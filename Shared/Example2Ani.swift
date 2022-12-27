4320372f5279dc4ec54a1625b9075e325b093d9c
JW Moon <moon910904@gmail.com>
2022-12-27 16:01:17 +0900

    블로그 최종

---
+++ Shared/Example2Ani.swift
@@ -0,0 +1,20 @@
+//
+//  Example2Ani.swift
+//  SwiftUI_Practice
+//
+//  Created by JW Moon on 2022/12/27.
+//
+
+import SwiftUI
+
+struct Example2Ani: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Example2Ani_Previews: PreviewProvider {
+    static var previews: some View {
+        Example2Ani()
+    }
+}
---
+++ Shared/ExampleAni.swift
@@ -0,0 +1,20 @@
+//
+//  ExampleAni.swift
+//  SwiftUI_Practice
+//
+//  Created by JW Moon on 2022/12/27.
+//
+
+import SwiftUI
+
+struct ExampleAni: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExampleAni_Previews: PreviewProvider {
+    static var previews: some View {
+        ExampleAni()
+    }
+}
