46d51d8222031e2b18416728f10ed118148a705b
teddy <teddy@akaai.kr>
2022-12-29 11:30:10 +0900

    Color Asset 정의하기

---
+++ Shared/ColorAsset.swift
@@ -0,0 +1,20 @@
+//
+//  ColorAsset.swift
+//  SwiftUI_Practice
+//
+//  Created by Jong Won Moon on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ColorAsset: View {
+    var body: some View {
+        Color("Orange")
+    }
+}
+
+struct ColorAsset_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorAsset()
+    }
+}
